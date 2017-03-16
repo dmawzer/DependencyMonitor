515348936a402a11fdd068d28ad0907e0683895d
dmawzer <dmawzer@gmail.com>
2017-03-16 16:15:45 +0300

    output write on console

--- Sample.DependencyMonitor/Sample.DependencyMonitor.Console/Program.cs
+++ Sample.DependencyMonitor/Sample.DependencyMonitor.Console/Program.cs
@@ -8,15 +8,37 @@ namespace Sample.DependencyMonitor.Console
     {
         static void Main(string[] args)
         {
-            string dllPath = @"D:\BOA\client\bin\BOA.EOD.CoreBanking.Customer.dll";
+            string dllPath = @"D:\blabla.dll";
 
             Assembly assembly = Assembly.LoadFrom(dllPath);
             string namespacee = assembly.GetName().Name;
-            List<string> classList = assembly.GetTypes().Where(x => x.IsClass).Select(x => x.FullName).Distinct().ToList();
-            if (classList != null && classList.Count != 0)
+            System.Console.WriteLine("- Namespace: {0}", namespacee);
+
+            List<string> classList = assembly.GetTypes()
+                                                .Where(x => x.IsClass)
+                                                .Select(x => x.FullName)
+                                                .Distinct()
+                                                .ToList();
+
+            foreach (var item in classList)
             {
-                List<string> methodList = assembly.GetTypes().Where(x => x.IsClass && x.FullName == classList.FirstOrDefault()).FirstOrDefault().GetMethods().Select(x => x.Name).Distinct().ToList();
+                System.Console.WriteLine("      -> ClassName: {0}", item);
+
+                List<string> methodList = assembly.GetTypes()
+                                                    .Where(x => x.IsClass && x.FullName == item)
+                                                    .FirstOrDefault()
+                                                    .GetMethods()
+                                                    .Select(x => x.Name)
+                                                    .Distinct()
+                                                    .ToList();
+
+                foreach (var method in methodList)
+                {
+                    System.Console.WriteLine("          => MethodName: {0}", method);
+                }
             }
+
+            System.Console.ReadLine();
         }
     }
 }
