c96ef9aa9406da6877c2abc3f76cba4e78ecb159
dmawzer <dmawzer@gmail.com>
2017-03-16 15:54:18 +0300

    initial

---
+++ Sample.DependencyMonitor/Sample.DependencyMonitor.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Sample.DependencyMonitor.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dllPath = @"D:\BOA\client\bin\BOA.EOD.CoreBanking.Customer.dll";
+
+            Assembly assembly = Assembly.LoadFrom(dllPath);
+            string namespacee = assembly.GetName().Name;
+            List<string> classList = assembly.GetTypes().Where(x => x.IsClass).Select(x => x.FullName).Distinct().ToList();
+            if (classList != null && classList.Count != 0)
+            {
+                List<string> methodList = assembly.GetTypes().Where(x => x.IsClass && x.FullName == classList.FirstOrDefault()).FirstOrDefault().GetMethods().Select(x => x.Name).Distinct().ToList();
+            }
+        }
+    }
+}
